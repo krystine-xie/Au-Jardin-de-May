0c1af70aff8728a300f5e62cf401289a70643946
Krystine Xie <krystinex@gmail.com>
2021-06-10 11:38:27 -0700

    fixed user profile bug

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -127,14 +127,12 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.get(`/api/users/${id}`, config);
+    const { data } = await axios.get(`/api/users/${id}/`, config);
 
     dispatch({
       type: USER_DETAILS_SUCCESS,
       payload: data,
     });
-
-    localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
       type: USER_DETAILS_FAIL,
